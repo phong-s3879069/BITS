87b215325da3edd7b9f8cc53aea38fefb2557f83
s3879069 <s3879069@rmit.edu.vn>
2022-04-01 15:39:50 +0700

    create table of other coins

--- cryptoconnect/src/App.js
+++ cryptoconnect/src/App.js
@@ -4,12 +4,14 @@ import Navbar from './Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import MainPage from './components/main_page/MainPage';
+import Table_of_other_coins from './components/coin_item/Table_of_other_coins';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <MainPage/>
+      <Table_of_other_coins/>
     </div>
   );
 }

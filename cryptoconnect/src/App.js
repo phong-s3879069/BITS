99b858b34ab8d3244744ced67309c5f85ec96e0c
s3879069 <s3879069@rmit.edu.vn>
2022-04-01 15:09:16 +0700

    create login and signup button for navbar

--- cryptoconnect/src/App.js
+++ cryptoconnect/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Navbar from './Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import MainPage from './components/main_page/MainPage';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <MainPage/>
     </div>
   );
 }

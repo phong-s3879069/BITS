9a20bd91287fcbb34a146b5d30e936031101e29a
KhoiNguyen-281 <s3759804@rmit.edu.vn>
2022-04-08 17:58:09 +0700

    News folder

--- cryptoconnect/src/App.js
+++ cryptoconnect/src/App.js
@@ -9,22 +9,27 @@ import Table_of_other_coins from './components/coin_item/Table_of_other_coins';
 import Navbar from './Navbar';
 import Homepage from './Homepage';
 import Profile_page from './components/profile_page/Profile_page';
+import News_main_page from './components/News_page/News_main_page';
 function App() {
   return (
-    <div>
-      <Router>
-        <div>
-          <Navbar/>
-        </div>
-        <div>
-          <Routes>
-            <Route exact path="/" element={<Homepage/>}/>
-            <Route exact path="/mainpage" element={<MainPage/>} />
-            <Route exact path="/profile" element={<Profile_page/>}/>
-          </Routes>
-        </div>
-      </Router>
+    <div className="App">
+      <News_main_page></News_main_page>
     </div>
+    
+    // <div>
+    //   <Router>
+    //     <div>
+    //       <Navbar/>
+    //     </div>
+    //     <div>
+    //       <Routes>
+    //         <Route exact path="/" element={<Homepage/>}/>
+    //         <Route exact path="/mainpage" element={<MainPage/>} />
+    //         <Route exact path="/profile" element={<Profile_page/>}/>
+    //       </Routes>
+    //     </div>
+    //   </Router>
+    // </div>
   );
 }
 
---
+++ cryptoconnect/src/components/News_page/News_main_page.js
@@ -0,0 +1,47 @@
+import News_items from './News_items';
+import './News_main_page.css';
+
+export default function News_main_page() {
+    const count = [1, 2, 3, 4]
+
+    return (
+        <div>
+            <div class="news-main-page">
+                <div class="news-title">
+                    <div class="news-header">News for you!</div>
+                    <div class="news-descript">Catch up with the latest updates from around the world</div>
+                </div>
+            </div>
+            {/* <div class="tabs_container"> */}
+            <div class="container d-flex justify-content-start">
+                <ul class="nav nav-pills mb-3 row" id="pills-tab" role="tablist">
+                    <div class="tabs_items col-sm">
+                        <div class="tabs_text">All</div>
+                    </div>
+                    <div class="tabs_items col-sm">
+                        <div class="tabs_text">Crypto</div>
+                    </div>
+                    <div class="tabs_items col-sm">
+                        <div class="tabs_text">Currency</div>
+                    </div>
+                    <div class="tabs_items col-sm">
+                        <div class="tabs_text">Stocks</div>
+                    </div>
+                </ul>
+            </div>
+            <div class="container">
+                <div class="row">
+                    {count.map((count) => {
+                        return (
+                            <div class="col-lg">
+                                <News_items />
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+
+        </div>
+
+    )
+}

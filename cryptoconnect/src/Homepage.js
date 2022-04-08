c1b57927fbdfcbb7cb0a87df5f3ebe13aad3040d
s3879069 <s3879069@rmit.edu.vn>
2022-04-08 17:56:17 +0700

    Home Screen and Navbar

--- cryptoconnect/src/Homepage.js
+++ cryptoconnect/src/Homepage.js
@@ -1,112 +1,43 @@
-import weblogo from "./assets/weblogo.jpg";
 import document from "./assets/document.png";
 import forum from "./assets/forum.png";
-import searching from "./assets/searching.jpg"
-import questioning from "./assets/questioning.jpg"
-import personalizing from "./assets/personalizing.jpg"
 import startgif from "./assets/star.gif"
 import chattraderepeat from "./assets/chattraderepeat.png";
+import welcometowebsite from "./assets/welcometowebsite.mp4";
+import market from "./assets/market.png";
+import './Homepage.css';
 export default function Homepage() {
     return (
         <div>
-            <section id="carousel-intro" style={{
-                width: "100%",
-                height: "100vh",
-                background: "#000",
-                overflow: "hidden",
-                position: "relative"
-            }}>
-                <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" style={{
-                    position: "absolute",
-                    top: 0,
-                    right: 0,
-                    left: 0,
-                    bottom: 0
-                }}>
-                    <div class="carousel-indicators" style={{
-                        position: "absolute",
-                        right: 0,
-                        left: 0,
-                        bottom: 0
-                    }}>
+            <section>
+                <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
                         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
                         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
                     </div>
-                    <div class="carousel-inner" role="listbox" style={{
-                        position: "absolute",
-                        top: 0,
-                        right: 0,
-                        left: 0,
-                        bottom: 0
-                    }}>
-                        <div class="carousel-item active" style={{
-                            background: "rgba(0,0,0,1)", position: "absolute",
-                            top: 0,
-                            right: 0,
-                            left: 0,
-                            bottom: 0
-                        }}>
-                            <div class="carousel-caption d-none d-md-block" style={{ transform: "translateY(-50%)", bottom: "initial", top: "50%", overflow: "hidden" }}>
-                                <h2>Welcome to CryptoConnect</h2><br />
-                                <h4>A Cryoto Website</h4>
-                                <a class="btn btn-success rounded-pill mt-4 mt-lg-4 mt-md-4 mt-xl-4" href="#about" role="button" style={{ fontSize: "120%" }}>Get Started</a>
-                            </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
 
-                        </div>
-                        <div class="carousel-item" style={{
-                            background: "rgba(0,0,0,1)", position: "absolute",
-                            top: 0,
-                            right: 0,
-                            left: 0,
-                            bottom: 0
-                        }}>
-                            <img src={searching} class="d-block w-100" alt="..." style={{
-                                opacity: 0.3, width: "100%",
-                                height: "100%",
-                                objectFit: "cover"
-                            }} />
-                            <div class="carousel-caption d-none d-md-block" style={{ transform: "translateY(-50%)", bottom: "initial", top: "50%" }}>
-                                <h2>One-page Programming Language Functions</h2><br />
-                                <h5>We offer a one-page UI for each programming languages' lists of functions to help you save time of searching and reduce reloading effort</h5>
-                                <a class="btn btn-success rounded-pill mt-4 mt-lg-4 mt-md-4 mt-xl-4" href="#about" role="button" style={{ fontSize: "120%" }}>Get Started</a>
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5 class="animated fadeInDown">Chat. Trade. Learn. Repeat</h5>
+                                <p class="animated fadeInUp">Lorum</p>
+                                <p class="animated fadeInUp"><a href="#">Appoint</a></p>
                             </div>
                         </div>
-                        <div class="carousel-item" style={{
-                            background: "rgba(0,0,0,1)", position: "absolute",
-                            top: 0,
-                            right: 0,
-                            left: 0,
-                            bottom: 0
-                        }}>
-                            <img src={questioning} class="d-block w-100" alt="..." style={{
-                                opacity: 0.3, width: "100%",
-                                height: "100%",
-                                objectFit: "cover"
-                            }} />
-                            <div class="carousel-caption d-none d-md-block" style={{ transform: "translateY(-50%)", bottom: "initial", top: "50%" }}>
-                                <h2>Feel Free to Ask</h2><br />
-                                <h5>Our categorized small discussion forums for each topic give you a high chance ask and get answer as soon as possible</h5>
-                                <a class="btn btn-success rounded-pill mt-4 mt-lg-4 mt-md-4 mt-xl-4" href="#about" role="button" style={{ fontSize: "120%" }}>Get Started</a>
+                        <div class="carousel-item">
+
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5 class="animated fadeInDown">News about Cryptocurrency</h5>
+                                <p class="animated fadeInUp">Lorum</p>
+                                <p class="animated fadeInUp"><a href="#">Appoint</a></p>
                             </div>
                         </div>
-                        <div class="carousel-item" style={{
-                            background: "rgba(0,0,0,1)", position: "absolute",
-                            top: 0,
-                            right: 0,
-                            left: 0,
-                            bottom: 0
-                        }}>
-                            <img src={personalizing} class="d-block w-100" alt="..." style={{
-                                opacity: 0.3, width: "100%",
-                                height: "100%",
-                                objectFit: "cover"
-                            }} />
-                            <div class="carousel-caption d-none d-md-block" style={{ transform: "translateY(-50%)", bottom: "initial", top: "50%" }}>
-                                <h2>Chat in real-time</h2><br />
-                                <h5>Speak with traders from around the world and never trade alone again.</h5>
-                                <a class="btn btn-success rounded-pill mt-4 mt-lg-4 mt-md-4 mt-xl-4" href="#about" role="button" style={{ fontSize: "120%" }}>Get Started</a>
+                        <div class="carousel-item">
+
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5 class="animated fadeInDown">Feel free to ask</h5>
+                                <p class="animated fadeInUp">Lorum</p>
+                                <p class="animated fadeInUp"><a href="#">Appoint</a></p>
                             </div>
                         </div>
                     </div>
@@ -119,82 +50,67 @@ export default function Homepage() {
                         <span class="visually-hidden">Next</span>
                     </button>
                 </div>
+                <section> 
+                    <div class="video">
+                            <video playsInline="playsinline" autoPlay="autoplay" muted="muted" loop="loop">
+                                    <source src={welcometowebsite} type="video/mp4"></source>   
+                            </video>
+                    </div>
+                </section>
+
+            <section class="text-white" id="about" style={{ backgroundColor: "#000", paddingBottom: "5%", paddingTop: "3%", marginTop: "2%" }}>
+                <h1 className="text-center">Coin Market</h1>
+                <div class="section-header-break text-center">--------<img src={startgif} class="img-fluid " style={{ backgroundColor: "#fff", maxWidth: "1.2%", maxHeight: "1.2%" }} />--------</div>
+                <br />
+                <div className="text-center ms-5 me-5">
+                    <p>Update real-time Cryptocurrency Price</p>
+                    <div>
+                        <img src={chattraderepeat} class="img-fluid rounded" alt="..." />
+                    </div>
+                </div>
             </section>
 
-            <section class="text-white" id="about" style={{ backgroundColor:"#000", paddingBottom: "5%", paddingTop: "3%" }}>
+            <section class="text-white" id="about" style={{ backgroundColor: "#000", paddingBottom: "5%", paddingTop: "3%" }}>
                 <h1 className="text-center">Chat.Trade.Repeat</h1>
-                <div class="section-header-break text-center">--------<img src={startgif} class="img-fluid " style={{ backgroundColor:"#fff", maxWidth: "1.2%", maxHeight: "1.2%" }} />--------</div>
+                <div class="section-header-break text-center">--------<img src={startgif} class="img-fluid " style={{ backgroundColor: "#fff", maxWidth: "1.2%", maxHeight: "1.2%" }} />--------</div>
                 <br />
                 <div className="text-center ms-5 me-5">
                     <p>Nowadays there are numerous trading platforms due to the increase in trading demands. However, these platforms have some common problems which is the lack of a social site for traders to be able to share their ideas as well as a place where new beginners could learn from scratch. To encounter these problems, our group has come up with a solution, a platform where people can trade, discuss and learn new things about trading in general. Our expected outcome is to be able to serve those mentioned necessities.</p>
                     <div>
-                        <img src={chattraderepeat} class="img-fluid rounded" alt="..."/>
+                        <img src={chattraderepeat} class="img-fluid rounded" alt="..." />
                     </div>
                 </div>
             </section>
 
-            <section class="text-white" id="about" style={{ backgroundColor:"#000", paddingBottom: "5%", paddingTop: "3%" }}>
-                <h1 className="text-center">ABOUT US</h1>
-                <div class="section-header-break text-center">--------<img src={startgif} class="img-fluid " style={{ backgroundColor:"#fff", maxWidth: "1.2%", maxHeight: "1.2%" }} />--------</div>
+
+            <section class="text-white" id="about" style={{ backgroundColor: "#000", paddingBottom: "5%", paddingTop: "3%" }}>
+                <h1 className="text-center">News about Cryptocurrency</h1>
+                <div class="section-header-break text-center">--------<img src={startgif} class="img-fluid " style={{ backgroundColor: "#fff", maxWidth: "1.2%", maxHeight: "1.2%" }} />--------</div>
                 <br />
                 <div className="text-center ms-5 me-5">
-                    <p>Nowadays there are numerous trading platforms due to the increase in trading demands. However, these platforms have some common problems which is the lack of a social site for traders to be able to share their ideas as well as a place where new beginners could learn from scratch. To encounter these problems, our group has come up with a solution, a platform where people can trade, discuss and learn new things about trading in general. Our expected outcome is to be able to serve those mentioned necessities.</p>
-                    <img src={weblogo} class="img-fluid mt-3"/>
+                    <p>We deliver up-to-date, breaking crypto news about the latest Bitcoin, Ethereum, Blockchain, NFTs, and Altcoin trends and happenings.</p>
+                    <div>
+                        <img src={chattraderepeat} class="img-fluid rounded" alt="..." />
+                    </div>
                 </div>
             </section>
 
-            <section id="services" style={{ backgroundColor: "#fff", paddingBottom: "5%", paddingTop: "3%" }}>
-                <h1 className="text-center">OUR SERVICES</h1>
-                <div class="section-header-break text-center">--------<img src={startgif} class="img-fluid" style={{ maxWidth: "1.2%", maxHeight: "1.2%" }} />--------</div>
+            <section class="text-white" id="about" style={{ backgroundColor: "#000", paddingBottom: "5%", paddingTop: "3%" }}>
+                <h1 className="text-center">Learn how to trade</h1>
+                <div class="section-header-break text-center">--------<img src={startgif} class="img-fluid " style={{ backgroundColor: "#fff", maxWidth: "1.2%", maxHeight: "1.2%" }} />--------</div>
                 <br />
-                <div class="row">
-                    <div class="col-sm-2"></div>
-                    <div class="col-sm-4">
-                        <div class="card text-center rounded-3" style={{ backgroundColor: "#f2efdb" }}>
-                            <div class="card-body">
-                                <img src={document} class="img-fluid w-25 h-25" alt="..." /><br /><br />
-                                <h5 class="card-title">Market</h5>
-                                <p class="card-text">Update Cryptocurrency Price</p>
-                                <a href="/cheatsheet" class="btn btn-outline-secondary" style={{ backgroundColor: "#8c8a8b", outlineColor: "#8c8a8b", color: "#f2efdb" }}>Go Explore</a>
-                            </div>
-                        </div>
+                <div className="text-center ms-5 me-5">
+                    <p>Get creative with the world markets by being part of the largest social network on the web for traders and investors. Discorver greate ideas, then publish your own</p>
+                    <div>
+                        <img src={chattraderepeat} class="img-fluid rounded" alt="..." />
                     </div>
-                    <div class="col-sm-4">
-                        <div class="card text-center rounded-3" style={{ backgroundColor: "#f2efdb" }}>
-                            <div class="card-body">
-                                <img src={forum} class="img-fluid w-25 h-25" alt="..." /><br /><br />
-                                <h5 class="card-title">Discussion Forum</h5>
-                                <p class="card-text">Feel free to post questions here</p>
-                                <a href="/discussion" class="btn btn-outline-secondary" style={{ backgroundColor: "#8c8a8b", outlineColor: "#8c8a8b", color: "#f2efdb" }}>Go Explore</a>
-                            </div>
-                        </div>
-                    </div>   
-                    <div class="col-sm-2"></div>
                 </div>
-                <div class="row mt-5">
-                    <div class="col-sm-2"></div>
-                    <div class="col-sm-4">
-                        <div class="card text-center rounded-3" style={{ backgroundColor: "#f2efdb" }}>
-                            <div class="card-body">
-                                <img src={document} class="img-fluid w-25 h-25" alt="..." /><br /><br />
-                                <h5 class="card-title">Market</h5>
-                                <p class="card-text">Update Cryptocurrency Price</p>
-                                <a href="/cheatsheet" class="btn btn-outline-secondary" style={{ backgroundColor: "#8c8a8b", outlineColor: "#8c8a8b", color: "#f2efdb" }}>Go Explore</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-4">
-                        <div class="card text-center rounded-3" style={{ backgroundColor: "#f2efdb" }}>
-                            <div class="card-body">
-                                <img src={forum} class="img-fluid w-25 h-25" alt="..." /><br /><br />
-                                <h5 class="card-title">Discussion Forum</h5>
-                                <p class="card-text">Feel free to post questions here</p>
-                                <a href="/discussion" class="btn btn-outline-secondary" style={{ backgroundColor: "#8c8a8b", outlineColor: "#8c8a8b", color: "#f2efdb" }}>Go Explore</a>
-                            </div>
-                        </div>
-                    </div>   
-                    <div class="col-sm-2"></div>
-                </div>
+            </section>
+
+            <section class="text-white" id="about" style={{ backgroundColor: "#000", paddingBottom: "5%", paddingTop: "3%" }}>
+                <h1 className="text-center">ABOUT US</h1>
+                <div class="section-header-break text-center">--------<img src={startgif} class="img-fluid " style={{ backgroundColor: "#fff", maxWidth: "1.2%", maxHeight: "1.2%" }} />--------</div>
+                <br />
             </section>
 
             <section id="contact" style={{ paddingBottom: "5%", paddingTop: "3%" }}>
@@ -230,6 +146,7 @@ export default function Homepage() {
                     </div>
                 </div>
 
+            </section>
             </section>
         </div>
     )

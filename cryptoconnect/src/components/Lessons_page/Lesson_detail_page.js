16cc318ce90c9175a6438255e7d23a857e7422df
Tống Nhật Quân <s3819347@rmit.edu.vn>
2022-04-20 14:42:22 +0700

    Applying some changes to the lesson

--- cryptoconnect/src/components/Lessons_page/Lesson_detail_page.js
+++ cryptoconnect/src/components/Lessons_page/Lesson_detail_page.js
@@ -11,6 +11,7 @@ function Lessons_detail_page(){
         <div class="text-muted image">Image</div>
         <div class="text-muted text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget elementum nisl. Morbi pellentesque arcu non metus volutpat facilisis. Quisque lobortis sagittis purus vel porttitor. Mauris feugiat, justo et sollicitudin sollicitudin, leo ex interdum augue, in fringilla tortor diam eu turpis. Nam id elit libero. Nulla sem lorem, egestas nec ante vel, eleifend feugiat quam. Duis ultrices, justo sed cursus elementum, tellus ex vulputate arcu, ut ullamcorper elit ex facilisis nulla. Nam ultrices ante nulla, quis tincidunt tellus interdum nec. Nulla facilisi. Nam eleifend dignissim sem at tempor. Duis consectetur pretium ex a sagittis. Proin leo orci, scelerisque non odio id, finibus aliquet est. Quisque ut mauris lectus. Praesent pharetra sagittis mauris, sit amet luctus nisi ultricies in. Sed non sem bibendum, vestibulum magna ut, condimentum ipsum.</div>
         <textarea class="comment" rows="3" placeholder="Leave a comment"></textarea>
+        <button class="edit" id = 'edit'>Edit this post</button>
       </div>
     </form>
     )}
--- cryptoconnect/src/components/Lessons_page/Lesson_edit.js
+++ cryptoconnect/src/components/Lessons_page/Lesson_edit.js
@@ -1,20 +1,33 @@
 import './Lesson_edit.css'
 export default function Lesson_edit() {
     return (
-        <form>
-      <div class="form-container">
-       <div class="form-header text-muted"> Edit Lesson post</div> 
-       <div class="card-header-container">
-            <textarea class="date" rows="1" placeholder="Change date"></textarea>
-            <textarea class="author" rows="1" placeholder="Change the author"></textarea>
-        </div>
-        <textarea class="comment mt-3" rows="3" placeholder="Leave a comment"></textarea>
-        <div class="button-container">
-        <button class="delete">Delete</button>
-        <button class="save">Save</button>
-        </div>
-        
+      <form>
+      <div className='edit-container'>
+          <div className= 'edit-header'>Edit lesson</div>
+          <div className = 'form-group' style={{backgroundColor: 'transparent'}}>
+              <div className = 'edit-body-title'>TITLE</div>
+              <textarea className = "form-control"  id='contentInput' rows="1" placeholder='Enter title'
+              />
+          </div>
+          <div className = 'form-group' style={{backgroundColor: 'transparent'}}>
+              <div className ='edit-category'>Category</div>
+              <select className = 'form-control' style= {{width: '15%', marginBottom: '2%'}} id='categorySelect'>
+                  <option>Category 1</option>
+                  <option>Category 2</option>
+                  <option>Category 3</option>
+                  <option>Category 4</option>
+              </select>
+          </div>
+          <div className = 'form-group' style={{backgroundColor: 'transparent'}}>
+              <div className = 'edit-body-title'>BODY</div>
+              <textarea className = "form-control" id='contentInput' rows="15" placeholder='Write something here'/>
+          </div>
+          <div className = 'form-group' style={{backgroundColor: 'transparent'}}>
+              <input type='file' className = 'form-control-file edit-button-container' id='addImage' placeholder = 'Add image'/>
+              <button type='submit' className = 'edit-create-button edit-button-container' id = 'create'>Edit</button>
+              <button type='delete' className = 'edit-create-button edit-button-container' id = 'delete'>Delete</button>
+          </div>
       </div>
-    </form>
+  </form>
     )
 }
